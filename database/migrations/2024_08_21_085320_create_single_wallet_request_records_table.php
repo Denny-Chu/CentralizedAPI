eaf7efcd74f8ed64556df30f03fcd586dc4eeb5b
denny <denny061008@gmail.com>
2024-09-04 16:09:26 +0800

    fix: parameter type of table: request

--- database/migrations/2024_08_21_085320_create_single_wallet_request_records_table.php
+++ database/migrations/2024_08_21_085320_create_single_wallet_request_records_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
             $table->string('method')->comment('單一錢包對象方法，例如auth, balance, etc.');
             $table->text('full_request')->comment('完整的請求內容');
             $table->string('request_method')->comment('HTTP請求方法（GET, POST等）');
-            $table->string('request_url')->comment('請求的完整URL');
+            $table->text('request_url')->comment('請求的完整URL');
             $table->timestamps(); // 創建和更新時間戳
         });
     }
